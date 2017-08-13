c748867c09d47570fbd5b0a77aed7c5c1c309430
荆文征 <200739491@qq.com>
2017-08-13 16:42:14 +0800

    上传路径修改 配置

--- src/main/java/com/aimobier/config/MyWebMvcConfigurerAdapter.java
+++ src/main/java/com/aimobier/config/MyWebMvcConfigurerAdapter.java
@@ -1,18 +1,32 @@
 package com.aimobier.config;
 
+import com.aimobier.util.PathUtil;
+import org.springframework.boot.web.servlet.MultipartConfigFactory;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.PathMatchConfigurer;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
+import javax.servlet.MultipartConfigElement;
+
 @Configuration
 public class MyWebMvcConfigurerAdapter extends WebMvcConfigurerAdapter{
+
+    /**
+     * 文件上传临时路径
+     */
+    @Bean
+    MultipartConfigElement multipartConfigElement() {
+        MultipartConfigFactory factory = new MultipartConfigFactory();
+        factory.setLocation(PathUtil.UPLOAD_TMP_FILE_PATH());
+        return factory.createMultipartConfig();
+    }
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         super.addResourceHandlers(registry);
-
     }
 
     @Override
