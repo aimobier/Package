2e416c3b5da3db0e7b79c92df4ad9dc5c5ffdc60
荆文征 <200739491@qq.com>
2017-08-14 18:35:27 +0800

    规范方法

--- src/main/java/com/aimobier/util/PathUtil.java
+++ src/main/java/com/aimobier/util/PathUtil.java
@@ -19,8 +19,43 @@ public class PathUtil {
         return filePathStr;
     }
 
-    public final static String SHELL_EXAMPLE_PATH(){
 
-        return new File("").getAbsolutePath()+"/shell/";
+
+
+    public final static String RootPathString(){
+
+        return new File("").getAbsolutePath();
+    }
+
+
+    /// 老的 配置文件
+    public final static File OLD_INFO_PLIST_FILE(){
+
+        return new File(PathUtil.RootPathString()+"/info.plist");
+    }
+
+    /// 新的配置文件
+    public final static File NEW_INFO_PLIST_FILE(){
+
+        return new File(PathUtil.RootPathString()+"/config/info.plist");
+    }
+
+
+    /// Sehll 文件 执行命令字符串
+    public final static String SHELL_FILE_PATH_STRING(String fileName){
+
+        return PathUtil.RootPathString()+"/shell/"+fileName;
+    }
+
+    /// 配置文件夹
+    public final static String CLEAR_CONFIG_PATH_STRING(){
+
+        return PathUtil.RootPathString()+"/config/*";
+    }
+
+    /// 上传文件夹
+    public final static String CLEAR_UPLOAD_PATH_STRING(){
+
+        return PathUtil.RootPathString()+"/upload/*";
     }
 }
