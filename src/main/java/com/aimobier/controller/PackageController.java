f69d2c43551f8616ce4d24c9c06c34ac3b1df12e
荆文征 <200739491@qq.com>
2017-08-15 18:48:02 +0800

    抽象方法

--- src/main/java/com/aimobier/controller/PackageController.java
+++ src/main/java/com/aimobier/controller/PackageController.java
@@ -1,115 +1,72 @@
 package com.aimobier.controller;
 
 import com.aimobier.entity.FILETYPE;
-import com.aimobier.entity.IMAGETYPE;
 import com.aimobier.entity.OdditySetObject;
 import com.aimobier.util.PathUtil;
-import com.dd.plist.NSDictionary;
-import com.dd.plist.PropertyListFormatException;
-import com.dd.plist.PropertyListParser;
+import com.aimobier.util.Shell;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.socket.handler.TextWebSocketHandler;
-import org.xml.sax.SAXException;
 
+import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
-import javax.xml.parsers.ParserConfigurationException;
 import java.io.*;
-import java.text.ParseException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 
 @RestController
 public class PackageController extends TextWebSocketHandler{
 
     @Autowired
     private SimpMessagingTemplate webSocket;
+    private Future<?> taskFuture = null;
 
     @PostMapping(value = "/config")
-    @ResponseBody
-    public String handleFormUploadMethod(@Valid OdditySetObject setObject,BindingResult result) {
+    public ModelAndView handleFormUploadMethod(@Valid OdditySetObject setObject, BindingResult result, HttpServletResponse response) {
 
-        /// 清空以前的 文件
+
         String[] clearOldFiles = { PathUtil.FILEString(FILETYPE.SHELL,"clear.sh"), PathUtil.FILEString(FILETYPE.CONFIG,"*"), PathUtil.FILEString(FILETYPE.UPLOAD,"*")};
-        sehll(clearOldFiles);
+        Shell.shell(this.webSocket,clearOldFiles);
 
-        /// 上传 ICON 和 LAUNCH
-        this.makeIconAndLaunch(setObject);
+        setObject.makeIconAndLaunch(webSocket);
 
+        send("开始 ICON 文件的配置");
         /// 裁剪ICON
         String[] makeICON = { PathUtil.FILEString(FILETYPE.SHELL,"iconshear.sh"), PathUtil.FILEString(FILETYPE.UPLOAD,"icon.png"), PathUtil.FILEString(FILETYPE.CONFIG,"AppIcon.appiconset/")};
-        sehll(makeICON);
-
-        /// 复制 ICON 配置文件
+        Shell.shell(this.webSocket,makeICON);
         String[] configICON = { "cp","-rf", PathUtil.FILEString(FILETYPE.SHELL,"icon/Contents.json"), PathUtil.FILEString(FILETYPE.CONFIG,"AppIcon.appiconset/")};
-        sehll(configICON);
+        Shell.shell(this.webSocket,configICON);
+        send("完成 ICON 文件的配置");
 
-        return setObject.getStatusstyleni()+"---"+setObject.getStatusstyleno()+"--"+setObject.getIcon().getSize();
+
+        send("开始 LAUNCH 文件的配置");
+        /// 裁剪ICON
+        String[] makeLaunch = { PathUtil.FILEString(FILETYPE.SHELL,"launchshear.sh"), PathUtil.FILEString(FILETYPE.UPLOAD,"launch.png"), PathUtil.FILEString(FILETYPE.CONFIG,"LaunchImage.launchimage/")};
+        Shell.shell(this.webSocket,makeLaunch);
+        String[] configLaunch = { "cp","-rf", PathUtil.FILEString(FILETYPE.SHELL,"launch/Contents.json"), PathUtil.FILEString(FILETYPE.CONFIG,"LaunchImage.launchimage/")};
+        Shell.shell(this.webSocket,configLaunch);
+        send("完成 LAUNCH 文件的配置");
+
+//        new Thread(() -> {
+//
+//
+//
+//        }).start();
+
+
+        return new ModelAndView("redirect:/");
     }
-
 
     public void send(String message){
 
-        webSocket.convertAndSend("/topic/greetings",message);
-    }
+        if (message != null){
 
-    public void sehll(String[] command){
-
-        try {
-
-            Process process = Runtime.getRuntime().exec(command);
-
-            BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
-
-            String s;
-
-            while ((s = reader.readLine()) != null) {
-
-                send(s);
-            }
-        } catch (IOException e) {
-
-            send(e.getLocalizedMessage());
+            webSocket.convertAndSend("/topic/greetings",message);
         }
     }
 
-    public void makeIconAndLaunch(OdditySetObject setObject) {
-
-        try {
-
-            setObject.makeImage(IMAGETYPE.ICON);
-
-            send("上传 ICON 完成");
-
-        } catch (Exception e) {
-
-            send(e.getLocalizedMessage());
-        }
-
-        try {
-
-            setObject.makeImage(IMAGETYPE.LAUNCH);
-
-            send("上传 LAUNCH 完成");
-
-        } catch (Exception e) {
-
-            send(e.getLocalizedMessage());
-        }
-
-        try {
-
-            NSDictionary rootDict = (NSDictionary) PropertyListParser.parse(PathUtil.FILE(FILETYPE.ROOT,"info.plist"));
-
-            rootDict = setObject.fuseNSDictionary(rootDict);
-
-            PropertyListParser.saveAsXML(rootDict,PathUtil.FILE(FILETYPE.CONFIG,"info.plist"));
-
-            send("配置 info.plst 文件 成功");
-
-        } catch (Exception e) {
-
-            send(e.getLocalizedMessage());
-        }
-    }
 }
