c72ec5a15f4decbd10b74502b2350d3427ca35b0
荆文征 <200739491@qq.com>
2017-08-18 18:48:00 +0800

    增加 创建文件方法

--- src/main/java/com/aimobier/util/PathUtil.java
+++ src/main/java/com/aimobier/util/PathUtil.java
@@ -2,7 +2,7 @@ package com.aimobier.util;
 
 import com.aimobier.entity.FILETYPE;
 
-import java.io.File;
+import java.io.*;
 
 public class PathUtil {
 
@@ -23,5 +23,31 @@ public class PathUtil {
 
     public static String FILEString(FILETYPE type,String fileName){
         return PathUtil.FILE(type,fileName).getAbsolutePath();
+    }
+
+    /**
+     * 创建文件
+     * @param fileFullPathString  文件名称
+     * @param fileContent   文件内容
+     * @return  是否创建成功，成功则返回true
+     */
+    public static boolean createFile(String fileFullPathString,String fileContent){
+        Boolean bool = false;
+        File file = new File(fileFullPathString);
+        try {
+            //如果文件不存在，则创建新的文件
+            if(!file.exists()){
+                file.getParentFile().mkdirs();
+                file.createNewFile();
+                bool = true;
+            }
+            PrintWriter writer = new PrintWriter(fileFullPathString, "UTF-8");
+            writer.println(fileContent);
+            writer.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return bool;
     }
 }
